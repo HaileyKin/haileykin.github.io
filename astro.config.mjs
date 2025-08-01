b8995b15657493b13004d48493eef51f99da2e02
Hailey Kinsella <haileykinsella@gmail.com>
2025-08-01 17:49:43 -0600

    new changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
-  //site: 'https://haileykin.github.io',
+  site: "https://haileykin.github.io",
   vite: {
     plugins: [tailwindcss()],
   },
